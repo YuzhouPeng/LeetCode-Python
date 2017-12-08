ae73c00ef1dc4a55ce58a6f2e3981d4bf9b9e73b
macjiong <760783896@qq.com>
2017-12-08 12:07:44 +0000

    cmmm

--- DFSS1/start.sh
+++ DFSS1/start.sh
@@ -6,7 +6,7 @@ mkdir -p DirectoryServerFiles/8006
 mkdir -p DirectoryServerFiles/8009
 mkdir -p DirectoryServerFiles/8010
 
-# python src/Servers/directoryServer.py 8005 &
+python src/Servers/directoryServer.py 8005 &
 # python src/Servers/lockServer.py 8007 &
 python src/Servers/fileServer.py 8006 &
 python src/Servers/fileServer.py 8009 &
