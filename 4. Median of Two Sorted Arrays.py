6cf73bb5cb68a9d6fe8ded0365aa4c96cdbb1fa8
macjiong <760783896@qq.com>
2018-11-09 19:15:38 +0000

    add code

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -1,29 +1,32 @@
 class Solution(object):
+
+    def find_the_median_k(self, alist, blist,k):
+        lengtha = len(alist)
+        lengthb = len(blist)
+        if lengtha > lengthb:
+            return self.find_the_median_k(blist, alist, k)
+        if lengtha ==0:
+            return blist[k-1]
+        if k ==1:
+            return min(alist[0],blist[0])
+        parameter_a = min(k/2, lengtha)
+        parameter_b = k-parameter_a
+
+        if alist[parameter_a-1]<=blist[parameter_b-1]:
+            return self.find_the_median_k(alist[parameter_a:],blist,parameter_b)
+        else:
+            return self.find_the_median_k(alist,blist[parameter_b:],parameter_a)
+
+
     def findMedianSortedArrays(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: float
         """
-        Len1 = len(nums1)
-        Len2 = len(nums2)
-        mid1 = nums1[0]
-        mid2 = nums2[Len2-1]
-        i = j = 0
-        if Len1 == 0 and Len2 % 2 ==0:
-            return (nums2[Len2/2]+nums2[(Len2/2)-1])/2
-        elif Len2 == 0 and Len1 % 2 ==0:
-            return (nums1[Len1/2]+nums1[(Len1/2)-1])/2
-        elif Len1 == 0 and Len2 % 2 == 1:
-            return nums2[Len2/2]
-        elif Len2 == 0 and Len1 % 2 == 1:
-            return nums1[Len1/2]
-        elif Len1 == 0 and Len2 == 0:
-            return -1
-        def binarysearch(self,numLen1,numLen2):
-            if numLen1>Len1:
-                
-
-        while i < Len2 and j < Len1:
-            if mid1 < mid2:
-
+        length_alist = len(nums1)
+        length_blist = len(nums2)
+        if ((length_alist+length_blist)%2==1):
+            return self.find_the_median_k(nums1,nums2,(length_alist+length_blist)/2+1)
+        else:
+            return (self.find_the_median_k(nums1,nums2,(length_alist+length_blist)/2)+self.find_the_median_k(nums1,nums2,(length_alist+length_blist)/2+1))*0.5
