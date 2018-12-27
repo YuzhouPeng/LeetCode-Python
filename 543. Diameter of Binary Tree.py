10a4b4fb6bbf36ffbe476e0a538b6f2149f852f1
macjiong <760783896@qq.com>
2018-12-27 13:31:47 +0000

    add code

--- 543. Diameter of Binary Tree.py
+++ 543. Diameter of Binary Tree.py
@@ -6,11 +6,18 @@
 #         self.right = None
 
 class Solution:
-    def depth(self,root):
+    def traverse(self,root):
         if root==None:
             return 0
+        left = self.traverse(root.left)
+        right = self.traverse(root.right)
+        self.ans = max(self.ans,left+right)
+        return max(left,right)+1
     def diameterOfBinaryTree(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
+        self.ans=0
+        self.traverse(root)
+        return self.ans
