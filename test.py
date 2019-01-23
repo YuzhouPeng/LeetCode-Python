470a8643c40eb4b08ec5b47dfdff9b68180e3fa9
macjiong <760783896@qq.com>
2019-01-23 12:36:34 +0000

    add code

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-def fuc(b=1,c=0,d=2):
-    print(b,c,d)
 
-fuc()
+import re,collections
+
+wordstring = 'it was the best of times it was the worst of times '
+wordstring += 'it was the age of wisdom it was the age of foolishness'
+wordlist = wordstring.split()
+length = len(wordlist)
+wordfreq = collections.Counter(wordlist)
+Termfrequency = collections.namedtuple('terms','term tf')
+tflist = []
+for word in wordfreq:
+    key = word
+    value = wordfreq[key]
+    print("{}--{}".format(key,value))
+    tflist.append(Termfrequency(term=word,tf=float(value)/length))
+print('length is {}'.format(length))
+print(tflist)
