b5c04134ad46df3e0d7ee4d8897f5c95edff3f1e
YuzhouPeng <760783896@qq.com>
2019-11-07 18:29:21 +0800

    add answer

---
+++ 1040. Moving Stones Until Consecutive II.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numMovesStonesII(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: List[int]
+        """
+        A = stones
+        A.sort()
+        i, n, low = 0, len(A), len(A)
+        high = max(A[-1] - n + 2 - A[1], A[-2] - A[0] - n + 2)
+        for j in range(n):
+            while A[j] - A[i] >= n: i += 1
+            if j - i + 1 == n - 1 and A[j] - A[i] == n - 2:
+                low = min(low, 2)
+            else:
+                low = min(low, n - (j - i + 1))
+        return [low, high]
