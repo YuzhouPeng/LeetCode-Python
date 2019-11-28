a3078eba88a1dfba95117e796bf747d613028fea
YuzhouPeng <760783896@qq.com>
2019-11-28 23:40:29 +0800

    add answer

--- 974. Subarray Sums Divisible by K.py
+++ 974. Subarray Sums Divisible by K.py
@@ -9,7 +9,7 @@ class Solution(object):
         prefix = 0
         count = [1]+[0]*(K)
         for a in A:
-            prefix = (prefix+a)%k
+            prefix = (prefix+a)%K
             res+=count[prefix]
             count[prefix]+=1
         return res
